e9b3d5a7428b177eaa9b5fcc90bf4bf1b0e2aa33
Saurabh Pujari <saurabhpuj99@gmail.com>
2021-03-27 11:52:53 +0530

    ProRataSimulation()

---
+++ proRata.py
@@ -0,0 +1,24 @@
+import random
+import numpy
+import matplotlib.pyplot as plt
+from globalMemory import gmemory
+
+class ProRataSimulation:
+
+    def __init__(self):
+        self.total_commission = 0
+        self.computation_matrix = []
+        self.artist_revenue_list = []
+        self.artist_users_list = []
+        self.users_following_list = []
+        self.total_views_per_artist = [0]*gmemory.TOTAL_ARTISTS
+        self.total_views_per_user = []
+
+    def runSimulation():
+        pass
+
+    def generateViewShare():
+        pass
+
+    def calculateArtistRevenue():
+        pass
